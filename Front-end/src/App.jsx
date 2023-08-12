5bb92c367e8f6a720e514bcdb56dbaff57865f1b
Indrajit Pal <palindrajit10@gmail.com>
2023-08-12 12:35:40 +0530

    footer section added

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar/>
+
+      <Footer />
     </Router>
 
   )
