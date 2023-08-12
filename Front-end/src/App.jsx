19b33d55b3e8cf6fbb13174ed92ce4f8dcd7d3df
Indrajit Pal <palindrajit10@gmail.com>
2023-08-12 13:36:09 +0530

    login signup functionality added without isLoggedIn state reserved

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -3,12 +3,17 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <Router>
       <Navbar/>
-
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
       <Footer />
     </Router>
 
