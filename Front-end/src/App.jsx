0156fd35c363aba15e0145b6a7b76531b4920b2f
krishnendu roy <krishnendu.kalyani1@gmail.com>
2023-08-12 14:00:47 +0530

    Ntf

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -3,16 +3,26 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 import Contribute from "./components/Contribute/Contribute";
 
 function App() {
   return (
     <Router>
+<<<<<<< HEAD
       <Navbar />
       <Routes>
         <Route path="/contribute" element={<Contribute />} />
       </Routes>
+=======
+      <Navbar/>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+>>>>>>> bc2244825e3a87aeb54df6911a6d0cd31e38daf0
       <Footer />
     </Router>
   );
